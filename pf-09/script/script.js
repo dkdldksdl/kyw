c46d505c6f29e60f55547270193e4bc019253eeb
dkdldksdl <87400785+dkdldksdl@users.noreply.github.com>
2021-07-14 11:29:32 +0900

    Add files via upload

---
+++ pf-09/script/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    var x = 0
+    setInterval(function(){
+        var next = (x+1)%3;
+        $(".sliderList").find("div").eq(x).fadeOut();
+        $(".sliderList").find("div").eq(next).fadeIn();
+        x = next;
+    },3000);
+
+    $(".layerpopup").on("click",function(){
+        $("#popup").fadeIn();
+    $(".close").on("click",function(){
+        $("#popup").fadeOut();
+        });
+    });
+});
